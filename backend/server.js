43879b9d1ac60426d9e828c0729962530e480eae
abdullah <abdullahatk311@gmail.com>
2024-12-17 17:39:07 +0100

    backend boilerplate added
    create a boilerplate for backend using node/express

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+const app = express()
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+//Database Connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.err(err))
+
+app.get('/', (req, res) => {
+    res.send('Task Management App Backend Running...')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
