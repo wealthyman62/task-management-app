8659d1fa61e416c23aaa28639c05b6ebac19482e
abdullah <abdullahatk311@gmail.com>
2024-12-17 18:42:18 +0100

    integrate backend + frontend
    integrate backend and frontend parts

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const path = require('path')
 
 dotenv.config()
 const app = express()
@@ -9,6 +10,7 @@ const app = express()
 //Middleware
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Database Connection
 mongoose.connect(process.env.MONGO_URI)
@@ -16,6 +18,10 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.err(err))
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/v1', (req, res) => {
     res.send('Task Management App Backend Running...')
 })
 
